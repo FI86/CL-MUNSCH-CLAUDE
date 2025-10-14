b78e2af842d8d0a2e6c5ef3a1b4c438692eb9981
Twixy Twixy <twixy@free.fr>
2025-10-14 11:39:11 +0200

    preparations

---
+++ Cours-Exercices/cours/Collections.java
@@ -0,0 +1,62 @@
+/**
+ * Exemple sur les collections
+ */
+
+package cours_exercices.cours;
+
+// Classe principale
+public class Collections {
+    public static void main(String[] args) {
+    	/// LIST
+    	// Creation d'une liste de type List
+        
+        // Ajout d'un element au premier index disponible
+        
+        // Insert un element a l'index voulu
+        
+        // Creation d'une autre liste de type List charge avec le contenu de la premiere
+        
+        // Parcour avec une boucle for intelligente
+        
+        // Acces a un element de la liste via son index
+        
+		// Recupere le premier index de la liste ayant pour valeur "valeur 3"
+	    
+		// Recupere le dernier index de la liste ayant pour valeur "valeur 3"       
+        
+        // Recherche de l'existence d'un element dans une liste
+	    
+		// Taille de la liste
+	    
+		// Suppression de l'element a l'index 1
+	    
+		// Suppression de l'element ayant la valeur "valeur 3"
+	    
+		// Suppression de tous les elements de la liste
+	    
+		// Tableau de String charge
+	    
+		// Conversion du tableau en liste de type List
+    	
+    	// Sans passer par l'import de la classe Arrays
+    	
+		// Affichage de la nouvelle liste
+	    
+		// Creation d'une liste de type List 
+        
+    	
+		/// SET
+		// Definition d'une collection Set
+        
+        // Charge monAutreListe de type List dans monSet de type Set
+        
+        // Affichage du Set, on s'apercoit qu'il n'y a plus de doublons
+    	
+		// Conversion en Set d'une liste d'int de type List, il n'y a plus de doublons
+	    
+		// Ajout de la valeur 5, mais elle existe deja
+	    
+    	
+        // MAP
+    }
+}
---
+++ Cours-Exercices/cours/Scanners.java
@@ -0,0 +1,19 @@
+package cours_exercices.cours;
+
+public class Scanners {
+    public static void main(String[] args) {
+        	 // Affiche la question sans revenir a la ligne grace a print au lieu de println
+
+            // Vide le buffer et affiche ce buffer.
+
+            
+            // Attend une saisie et verifie si c'est un int. 
+
+            	// Si c'est un int on recupere sa valeur et on l'affiche.
+
+            	// Sinon, on sort de la boucle
+
+
+        // Fermeture du Scanner.
+    }
+}
