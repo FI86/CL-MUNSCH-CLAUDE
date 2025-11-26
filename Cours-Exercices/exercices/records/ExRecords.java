424aa1714a3244082396ecf27df911ade561b34b
Twixy Twixy <twixy@free.fr>
2025-11-26 18:35:27 +0100

    ajout exercice records

---
+++ Cours-Exercices/exercices/records/ExRecords.java
@@ -0,0 +1,38 @@
+/**
+ * Exercice sur les records
+ * 
+ * Creer un record Etudiant pour representer les informations d'un etudiant.
+ * Les informations pour un etudiant sont les suivantes :
+ *      - nom
+ *      - numeroEtudiant
+ *      - moyenne
+ * 
+ * Implementer une classe GestionEtudiants qui contient une liste d'etudiants et les methodes suivantes :
+ *      ajouterEtudiant         : Ajoute un etudiant a la liste.
+ *      afficherEtudiants       : Affiche les informations de tous les etudiants.
+ *      rechercherEtudiant      : Recherche un etudiant par son numero et affiche ses informations.
+ *      calculerMoyenneGenerale : Calcul et affiche la moyenne generale de tous les etudiants.
+ */
+
+package cours_exercices.exercices.records;
+
+// Classe de type record
+
+
+// Classe GestionEtudiants
+
+
+// Classe ExRecords
+public class ExRecords {
+    public static void main(String[] args) {
+        // Creation d'une instance de GestionEtudiants.
+        
+        // Ajout d'etudiants a la liste.
+        
+        // Affichage de la liste des etudiants.
+        
+        // Recherche d'un etudiant par numero.
+        
+        // Calcul de la moyenne generale.
+    }
+}
